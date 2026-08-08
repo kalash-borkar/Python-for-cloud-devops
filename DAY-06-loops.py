de218fe6716758b1c49c6fc517cd89f18596a982
Kalash Borkar <kalashborkar2000@gmail.com>
2026-08-08 22:51:13 +0530

    Added loops practice

---
+++ DAY-06-loops.py
@@ -0,0 +1,18 @@
+#while loop practice
+#Print number from 1 to 10 using while loop?
+
+count  = 1
+
+while count < 6: #Condition
+    print(count)
+    count += 1
+
+count = 6
+
+while count > 0:
+    print(count)
+    count -= 1
+else:
+    print('while loop ended')
+
+#For loop
